92f07140d4e54fa50c03b0141841dc96f9962085
fVaqueroG <33148541+fVaqueroG@users.noreply.github.com>
2024-10-14 05:21:04 -0600

    Update mirage.h

--- components/mirage/mirage.h
+++ components/mirage/mirage.h
@@ -18,7 +18,7 @@ class MirageClimate : public climate_ir::ClimateIR {
       : climate_ir::ClimateIR(MIRAGE_TEMP_MIN, MIRAGE_TEMP_MAX, 1.0f, true, true,
                               {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
                                climate::CLIMATE_FAN_HIGH},
-                              {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL}) {}
+                              {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL, climate::CLIMATE_SWING_BOTH}) {}
 
   void setup() override {
     climate_ir::ClimateIR::setup();
