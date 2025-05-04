8863f2e17e4ac636d3119e4f8da2c681b7d19113
fVaqueroG <33148541+fVaqueroG@users.noreply.github.com>
2025-05-04 06:40:54 -0600

    Update mirage.h

--- components/mirage/mirage.h
+++ components/mirage/mirage.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "esphome/components/climate_ir/climate_ir.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace mirage {
@@ -18,10 +19,23 @@ class MirageClimate : public climate_ir::ClimateIR {
       : climate_ir::ClimateIR(MIRAGE_TEMP_MIN, MIRAGE_TEMP_MAX, 1.0f, true, true,
                               {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
                                climate::CLIMATE_FAN_HIGH},
-                              {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL, climate::CLIMATE_SWING_BOTH}) {}
+                              {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL, climate::CLIMATE_SWING_BOTH},
+                              {climate::CLIMATE_PRESET_NONE, climate::CLIMATE_PRESET_ECO, climate::CLIMATE_PRESET_SLEEP, climate::CLIMATE_PRESET_BOOST}) {}
+
+  void set_sensor(sensor::Sensor *sensor) { 
+    this->sensor_ = sensor; 
+    this->sensor_->add_on_state_callback([this](float state) {
+      this->current_temperature = state;
+      this->publish_state();
+    });
+  }                              
 
   void setup() override {
     climate_ir::ClimateIR::setup();
+    // Initialize current temperature if sensor is available
+    if (this->sensor_) {
+      this->current_temperature = this->sensor_->state;
+    }
   }
 
  protected:
@@ -31,6 +45,8 @@ class MirageClimate : public climate_ir::ClimateIR {
   bool on_receive(remote_base::RemoteReceiveData data) override;
   /// Set the time of the last transmission.
   int32_t last_transmit_time_{};
+  /// The sensor used for getting current temperature
+  sensor::Sensor *sensor_{nullptr};
 };
 
 }  // namespace mirage
