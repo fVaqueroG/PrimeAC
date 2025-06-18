a14b33d91128250a16a38eda98a5542d2e39c31f
fVaqueroG <33148541+fVaqueroG@users.noreply.github.com>
2025-06-18 14:18:08 -0600

    Update climate.py

--- components/mirage/climate.py
+++ components/mirage/climate.py
@@ -4,7 +4,7 @@ from esphome.components import climate_ir
 from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
-CODEOWNERS = ["@glmnet"]
+CODEOWNERS = ["@fVaqueroG"]
 
 mirage_ns = cg.esphome_ns.namespace("mirage")
 MirageClimate = mirage_ns.class_("MirageClimate", climate_ir.ClimateIR)
