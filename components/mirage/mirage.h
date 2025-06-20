477dd77b450e28591e2999fbff8d52f3e6ca799b
fVaqueroG <33148541+fVaqueroG@users.noreply.github.com>
2025-06-20 11:03:26 -0600

    Update mirage.h

--- components/mirage/mirage.h
+++ components/mirage/mirage.h
@@ -12,15 +12,17 @@ namespace mirage {
 const float MIRAGE_TEMP_MAX = 32.0;
 const float MIRAGE_TEMP_MIN = 16.0;
 
-class MirageClimate : public climate_ir::ClimateIR {
+class MirageClimate : public esphome::climate_ir::ClimateIR {
  public:
   uint8_t swing_position = 0;
   MirageClimate()
-      : climate_ir::ClimateIR(temperature_min_(), temperature_max_(), 1.0f, true, true,
+      : climate_ir::ClimateIR(MIRAGE_TEMP_MIN, MIRAGE_TEMP_MAX, 1.0f, true, true,
                               {esphome::climate::CLIMATE_FAN_AUTO, esphome::climate::CLIMATE_FAN_LOW, esphome::climate::CLIMATE_FAN_MEDIUM,
-                               esphome::climate::CLIMATE_FAN_HIGH},
-                              {esphome::climate::CLIMATE_SWING_OFF, esphome::climate::CLIMATE_SWING_VERTICAL, esphome::climate::CLIMATE_SWING_HORIZONTAL, esphome::climate::CLIMATE_SWING_BOTH},
-                              {esphome::climate::CLIMATE_PRESET_NONE, esphome::climate::CLIMATE_PRESET_ECO, esphome::climate::CLIMATE_PRESET_SLEEP, esphome::climate::CLIMATE_PRESET_BOOST}) {}
+                                esphome::climate::CLIMATE_FAN_HIGH},
+                              {esphome::climate::CLIMATE_SWING_OFF, esphome::climate::CLIMATE_SWING_VERTICAL, esphome::climate::CLIMATE_SWING_HORIZONTAL,
+                                esphome::climate::CLIMATE_SWING_BOTH},
+                              {esphome::climate::CLIMATE_PRESET_NONE, esphome::climate::CLIMATE_PRESET_ECO, esphome::climate::CLIMATE_PRESET_SLEEP,
+                                esphome::climate::CLIMATE_PRESET_BOOST}) {}
 
   void set_sensor(sensor::Sensor *sensor) { 
     this->sensor_ = sensor; 
@@ -31,7 +33,7 @@ class MirageClimate : public climate_ir::ClimateIR {
   }                              
 
   void setup() override {
-    climate_ir::ClimateIR::setup();
+    esphome::climate_ir::ClimateIR::setup();
     // Initialize current temperature if sensor is available
     if (this->sensor_) {
       this->current_temperature = this->sensor_->state;
@@ -42,7 +44,7 @@ class MirageClimate : public climate_ir::ClimateIR {
   /// Transmit via IR the state of this climate controller.
   void transmit_state() override;
   /// Handle received IR Buffer
-  bool on_receive(remote_base::RemoteReceiveData data) override;
+  bool on_receive(esphome::remote_base::RemoteReceiveData data) override;
   /// Set the time of the last transmission.
   int32_t last_transmit_time_{};
   /// The sensor used for getting current temperature
